10e23664efb4ae2d1ccfdfa05abb8beb5bae1ddb
Bak René <bakrene1@gmail.com>
2022-02-21 13:31:13 +0100

    java

--- javascript.js
+++ javascript.js
@@ -1,2 +1,11 @@
 const benzinarak= ["1000 cm3-ig 7,6 l/100km", "1001-1500 cm3 között 8,6 l/100km", "1501-2000 cm3 között 9,5 l/100km", "2001-3000 cm3 között 11,4 l/100km","3001 cm3 fölött 13,3 l/100km" ]
 const dizelarak=["1500 cm3-ig 5,7 l/100km","1501-2000 cm3 között 6,7 l/100km", "2001-3000 cm3 között 7,6 l/100km", "3001 cm3 fölött 9,5 l/100km" ]
+
+function getuzemanyagtipus()
+{
+    var adat;
+    adat = document.getElementById("uzemanyagtipus");
+    if (adat="benzin"){
+        
+    }
+}
